c4238fee9fa669d083d1d07c57c2848eae9f4293
Yanmeeei <wyanmei@umich.edu>
2023-03-12 12:15:10 -0400

    disable click remove fog

--- Assets/Scripts/Ground/FogTileManager.cs
+++ Assets/Scripts/Ground/FogTileManager.cs
@@ -3,23 +3,23 @@ using UnityEngine.EventSystems;
 
 public class FogTileManager : MonoBehaviour
 {
-    private bool isClickActive;
-
-    private void Awake()
-    {
-        EventBus.Subscribe<ModifyPauseEvent>(e => isClickActive = !e.status);
-    }
-
-    private void Start()
-    {
-        isClickActive = true;
-    }
-
-    private void OnMouseDown()
-    {
-        if (isClickActive && !EventSystem.current.IsPointerOverGameObject())
-        {
-            gameObject.SetActive(false);
-        }
-    }
+    // private bool isClickActive;
+    //
+    // private void Awake()
+    // {
+    //     EventBus.Subscribe<ModifyPauseEvent>(e => isClickActive = !e.status);
+    // }
+    //
+    // private void Start()
+    // {
+    //     isClickActive = true;
+    // }
+    //
+    // private void OnMouseDown()
+    // {
+    //     if (isClickActive && !EventSystem.current.IsPointerOverGameObject())
+    //     {
+    //         gameObject.SetActive(false);
+    //     }
+    // }
 }
