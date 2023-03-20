947dc6c023619e8bfd69682ce20edc489b2b8a3f
Chengfan Li <lllllcf@umich.edu>
2023-03-19 22:35:58 -0400

    finish citizen building

---
+++ Assets/CitizenBuildingControl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CitizenBuildingControl : MonoBehaviour
+{
+    private bool is_building_citizen = false;
+    GameObject building;
+
+    public void change_status(GameObject _building)
+    {
+        building = _building;
+        is_building_citizen = true;
+    }
+
+    public bool get_status()
+    {
+        return is_building_citizen;
+    }
+
+    public void remove_from_list()
+    {
+        building.GetComponent<SoliderBuilding>().removeCitizen(gameObject);
+    }
+}
--- Assets/Scripts/Building/SoliderBuilding.cs
+++ Assets/Scripts/Building/SoliderBuilding.cs
@@ -8,37 +8,59 @@ public class SoliderBuilding : MonoBehaviour
 {
     private int maxSolider = 3;
     private List<GameObject> autoSoliders;
+    private float last_res = 0.0f;
     private void Start()
     {
         autoSoliders = new List<GameObject>();
-        if (autoSoliders.Count < maxSolider)
-        {
-            GameObject solider = Instantiate(Resources.Load<GameObject>("Prefabs/Objects/Citizen"), transform.position, Quaternion.identity);
-            solider.GetComponent<UnitRTS>().MoveTo(transform.position + generateRandomVector());
-            autoSoliders.Add(solider);
-        }
         StartCoroutine(GenerateSolider());
     }
 
     IEnumerator GenerateSolider()
     {
-        if (autoSoliders.Count < maxSolider)
+        while (true)
         {
-            yield return new WaitForSeconds(3.0f);
-            GameObject solider = Instantiate(Resources.Load<GameObject>("Prefabs/Objects/Citizen"), transform.position, Quaternion.identity);
-            solider.GetComponent<UnitRTS>().MoveTo(transform.position + generateRandomVector());
-            autoSoliders.Add(solider);
+            if (autoSoliders.Count < maxSolider)
+            {
+                yield return new WaitForSeconds(3.0f);
+                GameObject solider = Instantiate(Resources.Load<GameObject>("Prefabs/Objects/Citizen"), transform.position, Quaternion.identity);
+                solider.GetComponent<UnitRTS>().MoveTo(transform.position + generateRandomVector());
+                solider.GetComponent<CitizenBuildingControl>().change_status(gameObject);
+                autoSoliders.Add(solider);
+            }
+
+            yield return new WaitForSeconds(1.0f);
         }
-    
-        yield return new WaitForSeconds(1.0f);
+        
     }
 
     private Vector3 generateRandomVector()
     {
-        Random.InitState(System.DateTime.Now.Millisecond);
-        int x = UnityEngine.Random.Range(60, 200);
-        Random.InitState(System.DateTime.Now.Millisecond);
-        int y = UnityEngine.Random.Range(60, 200);
-        return new Vector3(x / 100, y / 100, 0.0f);
+        float new_res1, new_res2;
+        while(true)
+        {
+            new_res1 = Random.Range(0.6f, 2.0f);
+            if (new_res1 != last_res)
+            {
+                last_res = new_res1;
+                break;
+            }
+                
+        }
+        while (true)
+        {
+            new_res2 = Random.Range(0.6f, 2.0f);
+            if (new_res2 != last_res)
+            {
+                last_res = new_res2;
+                break;
+            }
+
+        }
+        return new Vector3(new_res1, new_res2, 0.0f);
+    }
+
+    public void removeCitizen(GameObject citizen)
+    {
+        autoSoliders.Remove(citizen);
     }
 }
