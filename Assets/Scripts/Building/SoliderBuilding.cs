2182f12bccaac4129084bdf26c8463028ddcea2e
Chengfan Li <lllllcf@umich.edu>
2023-03-19 22:04:12 -0400

    solider building

--- Assets/Scripts/Building/SoliderBuilding.cs
+++ Assets/Scripts/Building/SoliderBuilding.cs
@@ -11,23 +11,33 @@ public class SoliderBuilding : MonoBehaviour
     private void Start()
     {
         autoSoliders = new List<GameObject>();
-    }
-
-    private void Update()
-    {
         if (autoSoliders.Count < maxSolider)
         {
             GameObject solider = Instantiate(Resources.Load<GameObject>("Prefabs/Objects/Citizen"), transform.position, Quaternion.identity);
             solider.GetComponent<UnitRTS>().MoveTo(transform.position + generateRandomVector());
             autoSoliders.Add(solider);
         }
+        StartCoroutine(GenerateSolider());
+    }
+
+    IEnumerator GenerateSolider()
+    {
+        if (autoSoliders.Count < maxSolider)
+        {
+            yield return new WaitForSeconds(3.0f);
+            GameObject solider = Instantiate(Resources.Load<GameObject>("Prefabs/Objects/Citizen"), transform.position, Quaternion.identity);
+            solider.GetComponent<UnitRTS>().MoveTo(transform.position + generateRandomVector());
+            autoSoliders.Add(solider);
+        }
+    
+        yield return new WaitForSeconds(1.0f);
     }
 
     private Vector3 generateRandomVector()
     {
-        Random.seed = System.DateTime.Now.Millisecond;
+        Random.InitState(System.DateTime.Now.Millisecond);
         int x = UnityEngine.Random.Range(60, 200);
-        Random.seed = System.DateTime.Now.Millisecond;
+        Random.InitState(System.DateTime.Now.Millisecond);
         int y = UnityEngine.Random.Range(60, 200);
         return new Vector3(x / 100, y / 100, 0.0f);
     }
