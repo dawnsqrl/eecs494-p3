54434287396670dae2f134ea6dc3a71c92c5e6f6
Chengfan Li <lllllcf@umich.edu>
2023-03-19 22:53:57 -0400

    resource building

---
+++ Assets/ResourceBuilding.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceBuilding : MonoBehaviour
+{
+    [SerializeField] private GameObject vitality;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vitality.GetComponent<VitalityController>().decreaseVitality(100);
+        vitality.GetComponent<VitalityController>().increaseVitalityGrowth(10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
