2b79c23a5720c5c69acb8223033fd2035911b5a2
andrsh <andrsh@umich.edu>
2023-04-01 23:11:01 -0400

    finished mine

--- Assets/MineBehavior.cs
+++ Assets/MineBehavior.cs
@@ -5,27 +5,48 @@ using UnityEngine;
 public class MineBehavior : MonoBehaviour
 {
     // Start is called before the first frame update
-    float damage_radius = 2.0f;
+    float damage_radius = 5.0f;
     int damage = 3;
+    Animator animator;
+    bool explode_lock = false;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+   
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Citizen")){
             //TODO: mine explosion effect
-            foreach (var citizen in CitizenControl.citizenList)
+            if (!explode_lock) {
+                animator.SetTrigger("explode");
+                StartCoroutine(DestoryAll(gameObject));
+                explode_lock = true;
+            }
+            
+        }
+       
+    }
+
+    IEnumerator DestoryAll(GameObject gameObject) {
+        yield return new WaitForSeconds(0.3f);
+        foreach (var citizen in CitizenControl.citizenList)
+        {
+            Debug.Log(citizen.name);
+            if (citizen != null)
             {
-                if (citizen != null)
+                if (Vector3.Distance(citizen.transform.position, transform.position) < damage_radius)
                 {
-                    if (Vector3.Distance(citizen.transform.position, transform.position) < damage_radius)
-                    {
-                        citizen.GetComponentInChildren<HitHealth>().ReduceHealth(damage);
-                    }
+                    citizen.GetComponentInChildren<HitHealth>().ReduceHealth(damage);
                 }
             }
         }
-       
-      
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
     }
-
-
+   
 
 }
