9445aaa9da7c1b4d4f90af2f79f32edfe8b39f33
andrsh <andrsh@umich.edu>
2023-04-03 02:50:46 -0400

    fix bug

--- Assets/MineBehavior.cs
+++ Assets/MineBehavior.cs
@@ -10,10 +10,7 @@ public class MineBehavior : MonoBehaviour
     Animator animator;
     bool explode_lock = false;
 
-    //private void Awake()
-    //{
-    //    transform.GetChild(0).gameObject.SetActive(false);
-    //}
+   
     private void Start()
     {
         animator = GetComponent<Animator>();
