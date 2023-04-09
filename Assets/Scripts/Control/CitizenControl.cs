a7da2dc8efbff02fb57861ed0b66cb4dc13b2472
Yanmeeei <wyanmei@umich.edu>
2023-04-08 21:24:53 -0400

    update

--- Assets/Scripts/Control/CitizenControl.cs
+++ Assets/Scripts/Control/CitizenControl.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using CodeMonkey.Utils;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
@@ -26,5 +27,26 @@ public class CitizenControl : MonoBehaviour
         GameObject citizen = Instantiate(Resources.Load<GameObject>("Prefabs/Objects/Citizen"), UtilsClass.GetMouseWorldPosition(), Quaternion.identity);
         citizen.GetComponent<UnitRTS>().MoveTo(citizen.transform.position);
         citizenList.Add(citizen);
+    }
+    
+    public static GameObject NearestCitizen( Vector3 pos)
+    {
+        float leastDistance = 999;
+        GameObject res = null;
+        for (int i = 0; i < citizenList.Count; i++)
+        {
+            if (citizenList[i].IsDestroyed())
+            {
+                continue;
+            }
+
+            float distance = Vector3.Distance(pos, citizenList[i].transform.position);
+            if (distance < leastDistance)
+            {
+                leastDistance = distance;
+                res = citizenList[i];
+            }
+        }
+        return res;
     }
 }
