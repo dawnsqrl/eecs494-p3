e7f95178963222d2119bd2266426ffeae70ac253
andrsh <andrsh@umich.edu>
2023-04-08 23:30:44 -0400

    polished animation

--- Assets/ShieldBehavior.cs
+++ Assets/ShieldBehavior.cs
@@ -6,14 +6,19 @@ public class ShieldBehavior : MonoBehaviour
 {
     public float max_HP=20;
     public float HP;
+    public float cycle = 5;
+    SpriteRenderer renderer;
+    bool render_lock=false;
 
     // Update is called once per frame
     private void Start()
     {
         HP = max_HP;
+        renderer= GetComponent<SpriteRenderer>();
     }
     void Update()
     {
+        renderer.color = new Color(1, 1, 1, (80 * Mathf.Sin(2 * Mathf.PI * Time.time / cycle) + 160)/255);
         if (HP < 0) {
             HP = max_HP;
             gameObject.SetActive(false);
@@ -27,4 +32,16 @@ public class ShieldBehavior : MonoBehaviour
     public void GetFullHP() {
         HP = max_HP;
     }
+
+    //IEnumerator BlinkAnimation() {
+    //    if (!render_lock) {
+    //        render_lock = true;
+    //        renderer.color=new Color(255,255,255, 52 * Mathf.Sin(2 * Mathf.PI * Time.time / cycle) + 151);
+    //        yield return nu
+    //        render_lock = false;
+    //    }
+    //    else {
+    //        yield return null;
+    //    }
+    //}
 }
