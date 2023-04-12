1755f5a8f5f4d6e11a686c822d54c1eadedd46eb
andrsh <andrsh@umich.edu>
2023-04-12 16:05:26 -0400

    add shield effect & add health bar

--- Assets/Scripts/ShieldBehavior.cs
+++ Assets/Scripts/ShieldBehavior.cs
@@ -18,6 +18,9 @@ public class ShieldBehavior : MonoBehaviour
     bool canShield = false, activated = false;
     [SerializeField] private GameObject shieldGameObject;
     [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private SpriteRenderer healthBar;
+
+    private float original_bar_length;
 
     // Update is called once per frame
     private void Awake()
@@ -28,17 +31,21 @@ public class ShieldBehavior : MonoBehaviour
     private void Start()
     {
         HP = max_HP;
-        renderer = GetComponent<SpriteRenderer>();
+        renderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
         shieldGameObject.SetActive(false);
         numShield = 0;
         _text.text = numShield.ToString();
+        original_bar_length = healthBar.size.x;
+        healthBar.size =
+            new Vector2((float)HP / (float)max_HP * original_bar_length, healthBar.size.y);
     }
 
     void Update()
     {
+        renderer.color = new Color(1, 1, 1, (80 * Mathf.Sin(2 * Mathf.PI * Time.time / cycle) + 160) / 255);
         if (canShield && activated)
         {
-            renderer.color = new Color(1, 1, 1, (80 * Mathf.Sin(2 * Mathf.PI * Time.time / cycle) + 160) / 255);
+            
             if (HP < 0)
             {
                 shieldGameObject.SetActive(false);
@@ -61,12 +68,18 @@ public class ShieldBehavior : MonoBehaviour
             activated = true;
             shieldGameObject.SetActive(true);
             HP = max_HP;
+            healthBar.size =
+                new Vector2((float)HP / (float)max_HP * original_bar_length, healthBar.size.y);
         }
     }
 
     public void ReduceHP(float cnt) {
-        if (canShield)
+        if (canShield) {
             HP -= cnt;
+            healthBar.size =
+                new Vector2((float)HP / (float)max_HP * original_bar_length, healthBar.size.y);
+        }
+            
     }
 
     public void AddShield()
